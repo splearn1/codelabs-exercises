d27f268a96c7135c202e353e987f96a2b524c263
splearn1 <barescents4u@gmail.com>
2023-10-23 20:33:57 -0400

    updating files

--- Angular/project-angular-recipe/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Angular/project-angular-recipe/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
@@ -8,8 +8,13 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'https://tse4.mm.bing.net/th?id=OIP.1SnwUZukEnoCqTQG3FlV9QHaGd&pid=Api&P=0&h=220'),
-    new Recipe('Another Recipe', 'This is another test', 'https://webstockreview.net/images/cookbook-clipart-appetizer-1.jpg'),
+    new Recipe('Another Test Recipe', 'This is another test', 'https://webstockreview.net/images/cookbook-clipart-appetizer-1.jpg'),
   ];
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
